78b89cbaea7ee82acfe60e5561a7438a7b7eb4cc
YuraKholodov <110026379+YuraKholodov@users.noreply.github.com>
2023-06-13 17:16:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import psycopg2
 
 
-def create_db(conn):
+def create_db(cur):
     """Добавление таблиц в БД"""
-    cur = conn.cursor()
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS users(
             id SERIAL PRIMARY KEY,
-            first_name VARCHAR(50),
-            last_name VARCHAR(50),
-            email VARCHAR(50)
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL
         );
 
         CREATE TABLE IF NOT EXISTS phones(
             id SERIAL PRIMARY KEY,
-            phone VARCHAR(15),
+            phone VARCHAR(15) DEFAULT 'phone number not specified',
             user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
         );
         """
     )
-    conn.commit()
 
 
-def add_client(conn, first_name=None, last_name=None, email=None, phones=None):
+def add_client(cur, first_name=None, last_name=None, email=None, phones=None):
     """Добавление клиента в БД"""
-    cur = conn.cursor()
     cur.execute(
         """
         INSERT INTO users
@@ -50,8 +47,8 @@ def add_client(conn, first_name=None, last_name=None, email=None, phones=None):
         print(f"В PHONES добавлен телефон: {cur.fetchone()}")
 
 
-def add_phone(conn, client_id, phone):
-    cur = conn.cursor()
+def add_phone(cur, client_id, phone):
+    """Добавление телефона"""
     cur.execute(
         """
         INSERT INTO phones (phone, user_id) VALUES (%s, %s) RETURNING id, phone, user_id
@@ -62,25 +59,32 @@ def add_phone(conn, client_id, phone):
 
 
 def change_client(
-    conn, client_id, first_name=None, last_name=None, email=None, phones=None
+    cur, client_id, first_name=None, last_name=None, email=None, phones=None
 ):
-    cur = conn.cursor()
-    cur.execute(
-        """
-        UPDATE users SET first_name=%s, last_name=%s, email=%s WHERE id=%s;
+    """Изменение данных клиента"""
+    if first_name:
+        cur.execute(
+            """
+        UPDATE users SET first_name=%s WHERE id=%s;
         """,
-        (first_name, last_name, email, client_id),
-    )
+            (first_name, client_id),
+        )
 
-    cur.execute(
-        """
-        SELECT *
-        FROM users
-        WHERE id=%s;
+    if last_name:
+        cur.execute(
+            """
+        UPDATE users SET last_name=%s WHERE id=%s;
         """,
-        (client_id),
-    )
-    print(f"Изменены данные в USERS: {cur.fetchone()}")
+            (last_name, client_id),
+        )
+
+    if email:
+        cur.execute(
+            """
+        UPDATE users SET email=%s WHERE id=%s;
+        """,
+            (email, client_id),
+        )
 
     if phones:
         cur.execute(
@@ -100,9 +104,19 @@ def change_client(
         )
         print(f"Изменен телефон: {cur.fetchone()}")
 
+    cur.execute(
+        """
+        SELECT u.id, u.first_name, u.last_name, u.email, p.phone
+        FROM users as u JOIN phones as p ON u.id = p.user_id
+        WHERE u.id=%s;
+        """,
+        (client_id),
+    )
+    print(f"Изменены данные в USERS: {cur.fetchone()}")
 
-def delete_phone(conn, client_id, phone):
-    cur = conn.cursor()
+
+def delete_phone(cur, client_id, phone):
+    """Удаление телефона"""
     cur.execute(
         """
         DELETE FROM phones
@@ -113,8 +127,8 @@ def delete_phone(conn, client_id, phone):
     print(f"Удален номер: {cur.fetchone()}")
 
 
-def delete_client(conn, client_id):
-    cur = conn.cursor()
+def delete_client(cur, client_id):
+    """Удаление клиента по ID"""
     cur.execute(
         """
         DELETE FROM users
@@ -125,14 +139,33 @@ def delete_client(conn, client_id):
     print(f"Удален клиент: {cur.fetchone()}")
 
 
-def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+def find_client(cur, first_name=None, last_name=None, email=None, phone=None):
     """Поиск клиента"""
-    cur = conn.cursor()
+    if first_name is None:
+        first_name = "%"
+    else:
+        first_name = "%" + first_name + "%"
+
+    if last_name is None:
+        last_name = "%"
+    else:
+        last_name = "%" + last_name + "%"
+
+    if email == None:
+        email = "%"
+    else:
+        email = "%" + email + "%"
+
+    if phone is None:
+        phone = "%"
+    else:
+        phone = "%" + phone + "%"
+
     cur.execute(
         """
         SELECT us.id, us.first_name, us.last_name, us.email, STRING_AGG(p.phone, ' OR ')
         FROM users AS us JOIN phones AS p ON us.id = p.user_id
-        WHERE us.first_name=%s OR us.last_name=%s OR us.email=%s OR p.phone=%s
+        WHERE us.first_name LIKE %s AND us.last_name LIKE %s AND us.email LIKE %s AND p.phone LIKE %s
         GROUP BY us.id
         """,
         (first_name, last_name, email, phone),
@@ -145,49 +178,59 @@ def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
         )
 
 
-def drop_all_tables(conn):
+def drop_all_tables(cur):
     """Удаление всех таблиц из БД"""
-    cur = conn.cursor()
     cur.execute(
         """
         DROP TABLE phones;
         DROP TABLE users;
         """
     )
-    conn.commit()
 
 
-with psycopg2.connect(
-    database="clients_db", user="postgres", password="baraguz"
-) as conn:
-    drop_all_tables(conn)
+if __name__ == "__main__":
+    with psycopg2.connect(
+        database="clients_db", user="postgres", password="baraguz"
+    ) as conn:
+        with conn.cursor() as cur:
+            drop_all_tables(cur)
 
-    create_db(conn)
+            create_db(cur)
 
-    add_client(
-        conn,
-        first_name="Yura",
-        last_name="Kholodov",
-        email="yuyuyuyuy",
-        phones="3243432545",
-    )
-    add_client(conn, first_name="Misha", last_name="Kholodov", phones="2113124214")
-    add_client(conn, first_name="Фудзи", phones="000000000")
+            add_client(
+                cur,
+                first_name="Yura",
+                last_name="Kholodov",
+                email="yuyuyuyuy@mail.ru",
+                phones="3243432545",
+            )
+            add_client(
+                cur,
+                first_name="Misha",
+                last_name="Kholodov",
+                email="ddvsvsd@mail.ru",
+                phones="2113124214",
+            )
+            add_client(
+                cur,
+                first_name="Фудзи",
+                last_name="Yama",
+                email="dsdsv@mail.ru",
+                phones="000000000",
+            )
 
-    change_client(
-        conn=conn,
-        client_id="2",
-        first_name="Николай",
-        last_name="Зайцев",
-        phones="777-777",
-    )
+            change_client(
+                cur,
+                client_id="2",
+                first_name="Николай",
+                last_name="Зайцев",
+                phones="777-777",
+            )
 
-    add_phone(conn=conn, client_id="3", phone="12345-12345")
+            add_phone(cur, client_id="3", phone="12345-12345")
 
-    delete_phone(conn=conn, client_id="2", phone="777-777")
+            delete_phone(cur, client_id="2", phone="777-777")
 
-    delete_client(conn=conn, client_id="1")
+            delete_client(cur, client_id="1")
 
-    find_client(conn=conn, first_name="Фудзи")
-
-conn.close()
+            find_client(cur, first_name="Фудзи")
